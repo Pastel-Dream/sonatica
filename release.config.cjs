3a494bde50f4cfa2bd185b5c60feddf2c16ab77a
Jirayu <jirayu.s@jirayu.net>
2025-01-02 02:21:18 +0700

    chore(release): update release configuration and add semantic-release plugins for automated versioning and changelog generation

--- release.config.cjs
+++ release.config.cjs
@@ -1,3 +1,21 @@
 module.exports = {
-  branches: ["main", { name: "next", prerelease: true }],
-}
+	branches: ["main", { name: "next", prerelease: true }],
+	plugins: [
+		"@semantic-release/commit-analyzer",
+		"@semantic-release/release-notes-generator",
+		"@semantic-release/npm",
+		"@semantic-release/github",
+		[
+			"@semantic-release/changelog",
+			{
+				changelogFile: "docs/CHANGELOG.md",
+			},
+		],
+		[
+			"@semantic-release/git",
+			{
+				assets: ["docs/CHANGELOG.md"],
+			},
+		],
+	],
+};
