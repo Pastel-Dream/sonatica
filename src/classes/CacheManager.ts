d0f524daa5ca213abc8df71e09d600fa05d86133
Jirayu <jirayu.s@jirayu.net>
2025-01-01 14:50:36 +0700

    feat(CacheManager): add maxSize parameter to limit cache size

--- src/classes/CacheManager.ts
+++ src/classes/CacheManager.ts
@@ -10,10 +10,12 @@ export class CacheManager {
 	> = new Map();
 
 	private readonly ttl: number;
+	private readonly maxSize: number;
 
-	constructor(ttl: number = 30 * 60 * 1000) {
+	constructor(ttl: number = 30 * 60 * 1000, maxSize: number = 100) {
 		// default 30 minutes
 		this.ttl = ttl;
+		this.maxSize = maxSize;
 	}
 
 	public get(key: string): SearchResult | null {
@@ -29,6 +31,8 @@ export class CacheManager {
 	}
 
 	public set(key: string, result: SearchResult): void {
+		if (this.cache.size >= this.maxSize) this.cache.delete(this.cache.keys().next().value);
+
 		this.cache.set(key, {
 			result,
 			timestamp: Date.now(),
