da9430c92567213ea8de3f8913789f972a26c287
Jirayu <jirayu.s@jirayu.net>
2025-01-14 08:46:00 +0700

    chore: typing

--- src/sorter/leastUsedNode.ts
+++ src/sorter/leastUsedNode.ts
@@ -1,4 +1,6 @@
+import { Collection } from "@discordjs/collection";
 import { Sorter } from "../types/Sorter";
+import { Node } from "../classes/Node";
 
 /**
  * Sorts an array of nodes based on their usage.
@@ -10,7 +12,7 @@ import { Sorter } from "../types/Sorter";
  * @param {Array} nodes - The array of nodes to be sorted.
  * @returns {Array} - The sorted array of nodes.
  */
-const sorter: Sorter = (nodes) => {
+const sorter: Sorter = (nodes: Collection<string, Node>): Collection<string, Node> => {
 	return nodes
 		.filter((node) => node.connected)
 		.filter((node) => node.isEnabled)
